841a7276b3bb4aa3c4943457890db1418a7311c9
Derek Wilson <derek@derek-wilson.com>
2018-01-02 13:57:37 +0000

    First commit

---
+++ dbconfig.php
@@ -0,0 +1,28 @@
+<?php
+class Database
+{
+     
+    private $host = "localhost";
+    private $db_name = "../yii/pcf/protected/data/pcf.db";
+    private $username = "root";
+    private $password = "";
+    public $conn;
+     
+    public function dbConnection()
+	{
+     
+	    $this->conn = null;    
+        try
+		{
+            $this->conn = new PDO("sqlite:" . $this->db_name);
+			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
+        }
+		catch(PDOException $exception)
+		{
+            echo "Connection error: " . $exception->getMessage();
+        }
+         
+        return $this->conn;
+    }
+}
+?>
