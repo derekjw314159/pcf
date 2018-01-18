89c60f58ddce2412e264c80d86a19bef3bd02bdd
Derek Wilson <derek@derek-wilson.com>
2018-01-18 21:29:16 +0000

    playerlist.php

--- playerlist.php
+++ playerlist.php
@@ -1,3 +1,19 @@
+<?php
+session_start();
+require_once $_SERVER['DOCUMENT_ROOT'] . '/pcf/class.user.php';
+$user_home = new USER();
+
+if(!$user_home->is_logged_in()) {
+	$user_home->redirect('index.php');
+	}
+
+$stmt = $user_home->runQuery("SELECT * FROM tbl_users WHERE userID=:uid");
+$stmt->execute(array(":uid"=>$_SESSION['userSession']));
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,89 +21,79 @@
     <title>PHP CRUD Operations Using PDO Connection</title>
 
     <!-- Bootstrap CSS File  -->
-    <link rel="stylesheet" type="text/css" href="bootstrap-3.3.5-dist/css/bootstrap.css"/>
-	<!-- Bootstrap --> 
+    <link rel="sEtylesheet" type="text/css" href="bootstrap-3.3.5-dist/css/bootstrap.css"/>
 	<link href="bootstrap/css/bootstrap.css" rel="stylesheet" media="screen">
--	<link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
-<!--	<link href="assets/styles.css" rel="stylesheet" media="screen">
-	-->
+	<!-- Bootstrap --> 
+	<link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+	<link href="assets/styles.css" rel="stylesheet" media="screen">
+	
 	<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
 	<!--[if lt IE 9]>
 		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
-	
-	<style>
-		.modal .modal-dialog { width: 80%; }
-	</style>
 
+	<!-- Need to reconcile some differences between the two bootstrap files -->
+	<style>
+		/*.modal .modal-dialog { width: 80%; }
+		/*.tr {background-color: "#FFFFFF"; } */
+		.navbar-inner { background-color: #04386b;} /* BB&O Blue */
+	</style>
 
 </head>
 <body>
 
+<!-- Navbar -->
+<div class="navbar navbar-fixed-top">
+	<div class="navbar-inner">
+		<div class="container-fluid">
+			<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-bar"></span>
+			 <span class="icon-bar"></span>
+			 <span class="icon-bar"></span>
+			</a>
+			<a class="brand" href="#">Member Home</a>
+			<div class="nav-collapse collapse">
+				<ul class="nav pull-right">
+					<li class="dropdown">
+						<a href="#" role="button" class="dropdown-toggle" data-toggle="dropdown"> <i class="icon-user"></i> 
+						<?php echo $row['userEmail']; ?> <i class="caret"></i>
+						</a>
+						<ul class="dropdown-menu">
+							<li>
+								<a tabindex="-1" href="logout.php">Logout</a>
+							</li>
+						</ul>
+					</li>
+				</ul>
+				<ul class="nav">
+					<li class="active">
+						<a href="http://www.codingcage.com/">Coding Cage</a>
+					</li>
+					<li class="dropdown">
+						<a href="#" data-toggle="dropdown" class="dropdown-toggle">Tutorials <b class="caret"></b>
+
+						</a>
+						<ul class="dropdown-menu" id="menu1">
+							<li><a href="http://www.codingcage.com/search/label/PHP OOP">PHP OOP</a></li>
+							<li><a href="http://www.codingcage.com/search/label/PDO">PHP PDO</a></li>
+							<li><a href="http://www.codingcage.com/search/label/jQuery">jQuery</a></li>
+							<li><a href="http://www.codingcage.com/search/label/Bootstrap">Bootstrap</a></li>
+							<li><a href="http://www.codingcage.com/search/label/CRUD">CRUD</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="http://www.codingcage.com/2015/09/login-registration-email-verification-forgot-password-php.html">Tutorial Link</a>
+					</li>
+					
+					
+				</ul>
+			</div>
+			<!--/.nav-collapse -->
+		</div>
+	</div>
+</div>
+
 <!-- Content Section -->
 <div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <h1>PHP CRUD Operations Using PDO Connection</h1>
-        </div>
-    </div>
-
-
-
-        <div class="navbar navbar-fixed-top">
-            <div class="navbar-inner">
-                <div class="container-fluid">
-                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-bar"></span>
-                     <span class="icon-bar"></span>
-                     <span class="icon-bar"></span>
-                    </a>
-                    <a class="brand" href="#">Member Home</a>
-                    <div class="nav-collapse collapse">
-                        <ul class="nav pull-right">
-                            <li class="dropdown">
-                                <a href="#" role="button" class="dropdown-toggle" data-toggle="dropdown"> <i class="icon-user"></i> 
-								<?php //echo $row['userEmail']; ?> <i class="caret"></i>
-								<?php echo 'email'; ?> <i class="caret"></i>
-                                
-								</a>
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a tabindex="-1" href="logout.php">Logout</a>
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
-                        <ul class="nav">
-                            <li class="active">
-                                <a href="http://www.codingcage.com/">Coding Cage</a>
-                            </li>
-                            <li class="dropdown">
-                                <a href="#" data-toggle="dropdown" class="dropdown-toggle">Tutorials <b class="caret"></b>
-
-                                </a>
-                                <ul class="dropdown-menu" id="menu1">
-                                    <li><a href="http://www.codingcage.com/search/label/PHP OOP">PHP OOP</a></li>
-                                    <li><a href="http://www.codingcage.com/search/label/PDO">PHP PDO</a></li>
-                                    <li><a href="http://www.codingcage.com/search/label/jQuery">jQuery</a></li>
-                                    <li><a href="http://www.codingcage.com/search/label/Bootstrap">Bootstrap</a></li>
-                                    <li><a href="http://www.codingcage.com/search/label/CRUD">CRUD</a></li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="http://www.codingcage.com/2015/09/login-registration-email-verification-forgot-password-php.html">Tutorial Link</a>
-                            </li>
-                            
-                            
-                        </ul>
-                    </div>
-                    <!--/.nav-collapse -->
-                </div>
-            </div>
-        </div>
-
-
-
-
 	<div class="row">
         <div class="col-md-12">
             <div class="pull-right">
