5ca84ba96b6eb1d6b528baa01f860feef0b4a45c
Derek Wilson <derek@derek-wilson.com>
2018-05-30 07:43:28 +0100

    catchup

--- playerlist.php
+++ playerlist.php
@@ -1,7 +1,9 @@
 <?php
 session_start();
 require_once $_SERVER['DOCUMENT_ROOT'] . '/pcf/class.user.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/pcf/class.county.php';
 $user_home = new USER();
+$county_home = new COUNTY();
 
 if(!$user_home->is_logged_in()) {
 	$user_home->redirect('index.php');
@@ -9,7 +11,7 @@ if(!$user_home->is_logged_in()) {
 
 $stmt = $user_home->runQuery("SELECT * FROM tbl_users WHERE userID=:uid");
 $stmt->execute(array(":uid"=>$_SESSION['userSession']));
-$row = $stmt->fetch(PDO::FETCH_ASSOC);
+$userrow = $stmt->fetch(PDO::FETCH_ASSOC);
 
 /* Function selectinline
  * Function for inline bootstrap select statement
@@ -35,6 +37,26 @@ function inlineselect($name, $values, $options, $selected=NULL, $spanwidth=2, $t
 	echo $tb . "\t\t</select>" . PHP_EOL;
 	echo $tb . "\t</div>" . PHP_EOL;
 	}
+
+/* Logic for default county
+ */
+if (isset($_REQUEST['county'])){
+	$county = $_REQUEST['county'];
+	}
+else{
+	$county = $userrow['userCounty'];
+	}
+// Check it exists
+$data = $county_home->Read();
+$countyrow = $data[0];
+for($i=0; $i<count($data) ; $i++){
+	if ($county == $data[$i]['userCounty']){
+
+		}
+	}
+		
+
+
 
 ?>
 
@@ -84,7 +106,7 @@ function inlineselect($name, $values, $options, $selected=NULL, $spanwidth=2, $t
 				<ul class="nav pull-right">
 					<li class="dropdown">
 						<a href="#" role="button" class="dropdown-toggle" data-toggle="dropdown"> <i class="icon-user"></i> 
-						<?php echo $row['userEmail']; ?> <i class="caret"></i>
+						<?php echo $userrow['userEmail']; ?> <i class="caret"></i>
 						</a>
 						<ul class="dropdown-menu">
 							<li>
